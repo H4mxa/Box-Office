daff65ad1994733d7fe9d03f6b125509fffc973f
h4mxa <duke80800@gmail.com>
2021-11-28 17:49:22 +0500

    added show page

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import { Switch, Route } from "react-router-dom";
 // ---------- Pages ------------
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
+import Show from "./pages/Show";
 
-import "./index.css";
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
 
       <Route exact path="/starred" component={Starred} />
+
+      <Route exact path="/show/:id">
+        <Show />
+      </Route>
 
       <Route>
         <div>Not found 404</div>
